73ea2510b787ace1ad7b558ecad0904664fda469
HwangRock <157935545+HwangRock@users.noreply.github.com>
2024-12-28 17:08:20 +0900

    feat redis CRUD

--- src/main/java/com/example/cipherquest/service/StageService.java
+++ src/main/java/com/example/cipherquest/service/StageService.java
@@ -13,4 +13,39 @@ public class StageService {
     private StageRepository stageRepository;
 
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public StageService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    //redis create, update
+    public void setToRedis(String key, Object value) {
+        try{
+            redisTemplate.opsForValue().set(key, value);
+        }catch(Exception e){
+            log.error(e.getMessage());
+            throw new RuntimeException("Failed to set value to redis");
+        }
+    }
+
+    //redis read
+    public Object getFromRedis(String key){
+        try{
+            return redisTemplate.opsForValue().get(key);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            throw new RuntimeException("Failed to retrieve value from redis");
+        }
+    }
+
+    //redis delete
+    public void deleteFromRedis(String key){
+        try{
+            redisTemplate.delete(key);
+        }catch(Exception e){
+            log.error(e.getMessage());
+            throw new RuntimeException("Failed to delete value from redis");
+        }
+    }
 }
