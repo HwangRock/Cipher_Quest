d493c555092186dc10695e4d8a90d968b2d44cb5
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-01-13 00:01:49 +0900

    feat algorithm

---
+++ src/main/java/com/example/cipherquest/cipher/EccStrategy.java
@@ -0,0 +1,14 @@
+package com.example.cipherquest.cipher;
+
+import org.springframework.stereotype.Component;
+
+@Component("stage12")
+public class EccStrategy implements EncryptStrategy{
+    public String encrypt(String plainText, String key) {
+        return "test ECC encryption";
+    }
+
+    public String decrypt(String cipherText, String key) {
+        return "test ECC decryption";
+    }
+}
