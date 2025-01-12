dd520e11ce10737c6a605a006bd657771b37edf6
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-01-12 23:59:16 +0900

    Create DhStrategy.java

---
+++ src/main/java/com/example/cipherquest/cipher/DhStrategy.java
@@ -0,0 +1,14 @@
+package com.example.cipherquest.cipher;
+
+import org.springframework.stereotype.Component;
+
+@Component("stage11")
+public class DhStrategy implements EncryptStrategy {
+    public String encrypt(String plainText, String key) {
+        return "test DH encrypt";
+    }
+
+    public String decrypt(String cipherText, String key) {
+        return "test DH decrypt";
+    }
+}
