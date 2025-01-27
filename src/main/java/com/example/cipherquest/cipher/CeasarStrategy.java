2b843c88a1051bae03b3798220945e2899035b67
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-01-28 01:00:54 +0900

    fix: 시저암호 암호화 음수 키 처리

--- src/main/java/com/example/cipherquest/cipher/CeasarStrategy.java
+++ src/main/java/com/example/cipherquest/cipher/CeasarStrategy.java
@@ -19,7 +19,7 @@ public class CeasarStrategy implements EncryptStrategy{
         for(int i=0;i<fin;i++){
             char ch=plainText.charAt(i);
             if(ch>='a' &&ch<='z'){
-                cipherText.append((char) ((ch-'a'+k)%26+'a')); // 0~25로 만들고 k만큼 욺기고 다시 0~25로 만들고 아스키코드에 맞춤.
+                cipherText.append((char) ((ch-'a'+k+26)%26+'a')); // 0~25로 만들고 k만큼 욺기고 다시 0~25로 만들고 아스키코드에 맞춤.
             }
             else{
                 cipherText.append(ch); // 암호화가 필요없는 경우는 그대로 넣음.
