6968638d8e30b0132c132957b90a0e15ea496cfe
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-02-01 00:55:33 +0900

    feat: 레일 펜스 암호화 알고리즘

--- src/main/java/com/example/cipherquest/cipher/RailfenceStrategy.java
+++ src/main/java/com/example/cipherquest/cipher/RailfenceStrategy.java
@@ -6,7 +6,53 @@ import org.springframework.stereotype.Component;
 @Component("stage5")
 public class RailfenceStrategy implements EncryptStrategy {
     public String encrypt(String plainText, String key) {
-        return "test rail fence";
+        plainText=plainText.toLowerCase();
+        int fin=plainText.length();
+        StringBuilder cipherText=new StringBuilder();
+
+        int k;
+        try {
+            k = Integer.parseInt(key); // 문자열을 정수로 변환
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Ceasar 암호의 키는 정수여야 합니다. 제공된 키: " + key);
+        }
+
+        Character[][] rail=new Character[k][fin];
+        int row=0;
+        boolean down=true;
+        for(int i=0;i<fin;i++){ // rail fence 작성
+            char c=plainText.charAt(i);
+
+            rail[row][i]=c;
+            if(down){
+                if(row==k-1){
+                    down=false;
+                    row--;
+                }
+                else{
+                    row++;
+                }
+            }
+            else{
+                if(row==0){
+                    down=true;
+                    row++;
+                }
+                else{
+                    row--;
+                }
+            }
+        }
+
+        for(int i=0;i<k;i++){
+            for(int j=0;j<fin;j++){
+                if(rail[i][j] != null){
+                    cipherText.append(rail[i][j]);
+                }
+            }
+        }
+
+        return cipherText.toString();
     }
 
     public String decrypt(String cipherText, String key) {
