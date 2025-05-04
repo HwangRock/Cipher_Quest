0f13db1a97dfcf748107d65c3b602d9f900ebd37
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-05-04 23:29:22 +0900

    feat: ADFGVX 암호화 알고리즘 구현

--- src/main/java/com/example/cipherquest/cipher/AdfgvxStrategy.java
+++ src/main/java/com/example/cipherquest/cipher/AdfgvxStrategy.java
@@ -2,6 +2,7 @@ package com.example.cipherquest.cipher;
 
 import org.springframework.stereotype.Component;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.HashMap;
 
 @Component("stage6")
@@ -55,10 +56,47 @@ public class AdfgvxStrategy implements EncryptStrategy{
         }
 
         int col=ln/keyLen;
-        String st[][]=new String[col][keyLen];
-        
+        char st[][]=new char[col][keyLen];
+        for(int i=0;i<fin;i++){
+            int col2=i/keyLen;
+            int row2=i%keyLen;
+            char c=plainText.charAt(i);
+            st[col2][row2]=c;
+        }
 
-        return "test ADGFGVX encrypt";
+        // key를 사전순으로 정렬시켜서 어떻게 순열.
+        // hash를 이용해서 키를 정렬시킨 새로운 문자열과 키의 문자열의 문자를 매개로 해서 인덱스끼리 짝을 지음.
+
+        // 위의 hash를 이용해서 st의 열 별로 순열시켜서 암호화 완성.
+
+        HashMap<Character, Integer> keyIndexMap = new HashMap<>();
+        for (int i = 0; i < keyLen; i++) {
+            keyIndexMap.put(key.charAt(i), i);
+        }
+
+        char[] sortedKey = key.toCharArray();
+        Arrays.sort(sortedKey);
+
+        int[] permutation = new int[keyLen];
+        boolean[] visited = new boolean[keyLen];
+        for (int i = 0; i < keyLen; i++) {
+            for (int j = 0; j < keyLen; j++) {
+                if (!visited[j] && key.charAt(j) == sortedKey[i]) {
+                    permutation[i] = j;
+                    visited[j] = true;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < keyLen; i++) {
+            int colIdx = permutation[i];
+            for (int j = 0; j < col; j++) {
+                encryptedTxt.append(st[j][colIdx]);
+            }
+        }
+
+        return encryptedTxt.toString();
     }
 
     public String decrypt(String cipherText, String key) {
